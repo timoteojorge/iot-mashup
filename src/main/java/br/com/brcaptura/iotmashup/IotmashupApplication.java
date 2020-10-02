8cec3962545c46ce6bddf45eb12b65531b5edea7
timoteo-jorge <timoteo.jorge@kinesso.com>
2020-10-02 11:23:11 -0300

    first commit

---
+++ src/main/java/br/com/brcaptura/iotmashup/IotmashupApplication.java
@@ -0,0 +1,13 @@
+package br.com.brcaptura.iotmashup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotmashupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotmashupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/brcaptura/iotmashup/IotmashupApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brcaptura.iotmashup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotmashupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
